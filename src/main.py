728bec3b0b7397950ae53633e99f7682c2ff27db
rhoitjadhav <jadhavrohit497@gmail.com>
2022-10-07 20:24:37 +0530

    add apis router

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 
 # Modules
 from config import PROJECT_NAME
+from apis.apis import router as api_router
 
 app = FastAPI(title=PROJECT_NAME)
 
+app.include_router(api_router)
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
-
