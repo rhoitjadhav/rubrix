a918ba0a269f1b9314e379e3e780d57c258b0e37
rhoitjadhav <jadhavrohit497@gmail.com>
2022-10-07 18:12:53 +0530

    Elasticsearch configuration

--- src/config.py
+++ src/config.py
@@ -2,3 +2,9 @@ import os
 
 PROJECT_NAME = "Rubrix API"
 
+# ElasticSearch
+ELASTIC_HOST = os.getenv("ELASTIC_HOST", "35.90.35.220")
+ELASTIC_PORT = int(os.getenv("ELASTIC_PORT", "9200"))
+ELASTIC_USER = os.getenv("ELASTIC_USER", "elastic")
+ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD", "1234")
+
